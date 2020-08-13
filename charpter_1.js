952ab8cbbd243be7149a61ddbdd78f55c9247faf
重田 将人 <masato.0928.j@gmail.com>
2020-08-14 08:40:06 +0900

    ループ処理をメソッドに切り出し、インライン化_totalAmount

--- charpter_1.js
+++ charpter_1.js
@@ -42,22 +42,27 @@ function statement(invoice, plays) {
     }
 
     function totalVolumeCredits() {
-        let volumeCredits = 0
+        let result = 0
         for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf);
+            result += volumeCreditsFor(perf);
         }
+        return result
     }
 
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
 
+    let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         result +=  `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `Amount owed is ${usd(totalAmount)}\n`;
+    result += `Amount owed is ${usd(totalAmount())}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
 }
