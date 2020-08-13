0f93add0db973da988fa61ce700235b322143468
重田 将人 <masato.0928.j@gmail.com>
2020-08-14 07:35:47 +0900

    switch文をamountForとして抽出

--- charpter_1.js
+++ charpter_1.js
@@ -1,4 +1,27 @@
 function statement(invoice, plays) {
+
+    function amountFor(aPerformance, play) {
+        let result = 0
+        switch (play.type) {
+            case "tragedy":
+                    result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                    result += 3000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 50 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unkown type: ${play.type}`);
+        }
+        return result;
+    }    
+
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
@@ -10,35 +33,18 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
+        let thisAmount = amountFor(perf, play);
 
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount += 3000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 50 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unkown type: ${play.type}`);
-        }
+        // ボリューム特典のポイントを加算
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // 喜劇の時は10人につき、さらにポイントを加算
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        // 注文の内訳を出力
+        result +=  `${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
 
-    // ボリューム特典のポイントを加算
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // 喜劇の時は10人につき、さらにポイントを加算
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    // 注文の内訳を出力
-    result +=  `${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-
-    totalAmount += thisAmount;
+        totalAmount += thisAmount;
     }
+
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
