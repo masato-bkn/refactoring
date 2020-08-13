e6214ae95d743e8c413f568324ab33671944b2ff
重田 将人 <masato.0928.j@gmail.com>
2020-08-14 07:43:59 +0900

    amountForからplayパラメータを削除

--- charpter_1.js
+++ charpter_1.js
@@ -4,9 +4,9 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                     result = 40000;
                 if (aPerformance.audience > 30) {
@@ -21,7 +21,7 @@ function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unkown type: ${play.type}`);
+                throw new Error(`unkown type: ${playFor(aPerformance).type}`);
         }
         return result;
     } 
@@ -36,7 +36,7 @@ function statement(invoice, plays) {
         }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // ボリューム特典のポイントを加算
         volumeCredits += Math.max(perf.audience - 30, 0);
