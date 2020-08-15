24f1216cb1710ac31e713fe350f291794c23051c
重田 将人 <masato.0928.j@gmail.com>
2020-08-15 20:31:17 +0900

    volumeCreditsForを基底クラスとサブクラスに持たせた

--- createStatementData.js
+++ createStatementData.js
@@ -4,25 +4,10 @@ class PerformanceCalculator {
         this.play = aplay;
     }
     get amount() {
-        let result = 0
-        switch (this.play.type) {
-            case "tragedy":
-                    result = 40000;
-                if (this.Performance.audience > 30) {
-                    result += 1000 * (this.Performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                    result += 3000;
-                if (this.Performance.audience > 20) {
-                    result += 10000 + 50 * (this.Performance.audience - 20);
-                }
-                result += 300 * this.Performance.audience;
-                break;
-            default:
-                throw new Error(`unkown type: ${this.play.type}`);
-        }
-        return result;
+        throw new Error('サブクラスの責務');
+    }
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0);
     }
 }
 
@@ -46,7 +31,19 @@ export default function createStatementData(invoice, plays){
             return result;
         }
     }
-    class ComedyCalulator extends PerformanceCalculator {}
+    class ComedyCalulator extends PerformanceCalculator {
+        get amount() {
+            let result = 30000;
+            if (this.performance.audience > 20) {
+                result += 10000 + 500 * (this.performance.audience -20);
+            }
+            result += 300 * this.performance.audience;
+            return result;
+        }
+        get volumeCredits() {
+            return super.volumeCredits + Math.floor(this.performance.audience / 5);
+        }
+    }
 
     const statementData = {};
     statementData.customer = invoice.customer
@@ -60,9 +57,7 @@ export default function createStatementData(invoice, plays){
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
-        result.totalAmount = totalAmount(result)
-        result.totalVolumeCredits = totalVolumeCredits(result)
+        result.volumeCredits = calculator.volumeCredits(result);
         return result;
     }
     function playFor(aPerformance) {
@@ -75,12 +70,6 @@ export default function createStatementData(invoice, plays){
     //     // value-objectパターンかな??
     //     return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
     // }
-    function volumeCreditsFor(aPerformance) {
-        let result = 0; 
-        volumeCredits += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type) volumeCredits += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
     function totalAmount(data) {
         return data.performances.
             reduce((total, p) => total + p.amount, 0);
