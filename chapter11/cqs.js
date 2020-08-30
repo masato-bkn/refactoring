649ef86c555940fe24e3a6074246cc20e4195030
重田 将人 <masato.0928.j@gmail.com>
2020-08-30 17:22:02 +0900

    chapter11問い合わせと更新の分離 問い合わせと更新

--- chapter11/cqs.js
+++ chapter11/cqs.js
@@ -1,10 +1,19 @@
 function alertForMiscreant (people) {
+    if (findMiscreant(people) !== "") setOffAlart();
+}
+
+function findMiscreant (people) {
     for (const p of people) {
         if (p === "Don") {
-            setOffAlarms();
             return "Don";
         }
     }
     return "";
 }
 
+// const find = alertForMiscreant(people);
+find = findMiscreant(people)
+alertForMiscreant(people);
+
+// ruby
+// setOffAlart() if findMiscreant(people) != ''
