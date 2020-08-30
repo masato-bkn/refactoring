a7e47f61656fea28a5777c86708ce19aef00c01d
重田 将人 <masato.0928.j@gmail.com>
2020-08-30 11:46:28 +0900

    10章ポリモフィズムによる条件記述の置き換え

--- charpter_10/plymorphism.js
+++ charpter_10/plymorphism.js
@@ -1,1 +1,22 @@
-function plumges(birds) {};
+function plumges(birds) {
+    return Map(birds.map(b => [b.name, plumage(b)]))
+};
+function speeds(birds) {
+    return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]))
+}
+function plumge(bird) {
+    switch (bird.type) {
+        case 'EuropeanSwallow':
+            return "average";
+        case 'AfterSwallow':
+            // ...
+    }
+};
+function speed(bird) {
+    switch (bird.type) {
+        case 'EuropeanSwallow':
+            return 35;
+        case 'AfricanSwallow':
+            // ...
+    }
+}
