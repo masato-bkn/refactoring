b1e2667cfd72a70f5c9e5d111faaa60a1c527c6d
重田 将人 <masato.0928.j@gmail.com>
2020-08-30 13:44:17 +0900

    10章ポリモフィズムによる条件記述の置き換え(2) ファクトリメソッドを作成

--- charpter_10/plymorphism_2.js
+++ charpter_10/plymorphism_2.js
@@ -1,29 +1,3 @@
-function rating(voyage, history) {
-    const vpr = voyageProfitFactor(voyage, history);
-    const vr = voyageRisk(voyage);
-    const chr = captainHistoryRisk(voyage, history);
-    if (vpr * 3 > (vr + chr *2)) return "A";
-    else return "B";
-}
-
-function voyageRisk(voyage){}
-function captainHistoryRisk(voyage, history){}
-function hasChinaHistroy(history) {}
-function voyageProfitFactor(voyage, history){}
-
-const voyage = {zone: "west-indies", lenght: 10};
-const history = [
-    {zpne: "east-indies", profit: 5},
-    {zpne: "china",       profit: -2},
-    // ...
-]
-
-const myRating = rating(voyage, history);
-
-function rating(voyage, history) {
-    return Rating(voyage, history).value;
-}
-
 class Rating {
     constructor(voyage, history) {
         this.voyage = voyage;
@@ -51,5 +25,26 @@ class Rating {
 }
 
 class ExperiencedChinaRating extends Rating {
-    
+
 }
+
+function rating(voyage, history) {
+    return createRating(voyage, history).value;
+}
+
+function createRating(voyage, history) {
+    if  (/*china???*/) {
+        return ExperiencedChinaRating(voyage, history)
+    }
+    else {
+        return new Rating(voyage, history)
+    }
+}
+
+const voyage = {zone: "west-indies", lenght: 10};
+const history = [
+    {zpne: "east-indies", profit: 5},
+    {zpne: "china",       profit: -2},
+    // ...
+]
+const myRating = rating(voyage, history);
