f1fe611bd4d662cb3747391c21b31e737d49f890
重田 将人 <masato.0928.j@gmail.com>
2020-08-30 10:00:18 +0900

    10章特殊ケースの導入 雛形を作成

---
+++ charpter_10/null_object.js
@@ -0,0 +1,32 @@
+class Site {
+    get customer() {return this._customer;}
+}
+
+class Customer {
+    get name() {}
+    get billingPlan() {}
+    set billingPlan(arg) {}
+    get paymentHistory() {}
+}
+
+// client1
+const aCustomer = site.customer;
+// ...
+let customerName;
+if (aCustomer === "unKnown") customerName = "occupant";
+else customerName = aCustomer.name;
+
+// client2
+const plan = (aCustomer === "unknown") ?
+    registory.billingPlans.basic
+    : aCustomer.billingPlan;
+
+// client3
+if (aCustomer !== "unknown") aCustomer.billingPlan = newPlan
+
+// client4
+const weeksDelinquent = (aCustomer === "unknown") ?
+    0
+    : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+
+// 特殊ケース判定後の処理が広範囲に渡っている時は、特殊ケース用オブジェクトの出番
