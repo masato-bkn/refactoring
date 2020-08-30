4b7fe6095b7be5f2a0d869d32731bb376a3735e6
重田 将人 <masato.0928.j@gmail.com>
2020-08-30 13:31:25 +0900

    10章ポリモフィズムによる条件記述の置き換え(2) 関数をクラスとして抽出

--- charpter_10/plymorphism_2.js
+++ charpter_10/plymorphism_2.js
@@ -8,7 +8,7 @@ function rating(voyage, history) {
 
 function voyageRisk(voyage){}
 function captainHistoryRisk(voyage, history){}
-function hasChina(history) {}
+function hasChinaHistroy(history) {}
 function voyageProfitFactor(voyage, history){}
 
 const voyage = {zone: "west-indies", lenght: 10};
@@ -19,3 +19,33 @@ const history = [
 ]
 
 const myRating = rating(voyage, history);
+
+function rating(voyage, history) {
+    return Rating(voyage, history).value;
+}
+
+class Rating {
+    constructor(voyage, history) {
+        this.voyage = voyage;
+        this.history = history;
+    }
+    get value() {
+        const vpr = this.voyageProfitFactor(voyage, history);
+        const vr = this.voyageRisk(voyage);
+        const chr = this.captainHistoryRisk(voyage, history);
+        if (vpr * 3 > (vr + chr *2)) return "A";
+        else return "B";
+    }
+    get voyageRisk() {
+
+    }
+    get captainHistoryRisk() {
+
+    }
+    get voyageProfitFactor() {
+
+    }
+    get hasChinaHistory() {
+
+    }
+}
