50dd08a05ff0aa2019e2a0c0f0502e57880f41d8
重田 将人 <masato.0928.j@gmail.com>
2020-09-06 13:53:12 +0900

    chapter07 オブジェクトによるプリミティブの置き換え priorityをvalueインスタンス化

--- chapter_7/translate_primitive_to_pbject.js
+++ chapter_7/translate_primitive_to_pbject.js
@@ -3,17 +3,21 @@ class Order {
         this.priority = data.priority;
         // ...
     }
-    get priority() { return this._priority; }
-    set priority(aString) { this._priority = aString; }
+    get priority() {return this._priority;}
+    get priorityString() { return this._priority.toString(); }
+    set priority(aString) { this._priority = new Priority(aString); }
 }
 
 class Priority {
-    constructor(value) {this._value = value;}
+    constructor(value) {
+        if (value instanceof Priority) return value;
+        this._value = value;
+    }
     toString() {return this._value;}
 }
 
 // client
 hightPriorityCount = orders.filter(
     // これをオブジェクトで表現したい
-    o => "high" === o.priority || "rush" === o.priority
+    o => "high" === o.priority.toString() || "rush" === o.priority.toString()
     )
